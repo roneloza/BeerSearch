7c2eeaeb210126506bd550d8b73b0db09aecb95c
Rone Shender Loza Aliaga <rone@b89.io>
2021-11-18 16:20:21 -0500

    Initial Commit

---
+++ BeerSearch/BeerSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerSearchApp.swift
+//  BeerSearch
+//
+//  Created by Rone Shender on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BeerSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
